50f09c1d56bb06586e86ef3b031c9d531fd91543
羅承志 <a2200099@yahoo.com>
2021-05-12 16:50:39 +0800

    Initial Commit

---
+++ randomNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomNumber
+//
+//  Created by 羅承志 on 2021/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
